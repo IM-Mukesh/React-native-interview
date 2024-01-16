899d7988a1e88a3b82c5c69d487c6ece1755a310
Mukesh Kumar <mukesh.kumar@borderfree.io>
2024-01-16 21:14:19 +0530

    second commit after making second screen

--- helpify/src/BioPage.js
+++ helpify/src/BioPage.js
@@ -1,59 +1,74 @@
 import React from 'react';
 import {
   View,
-  Button,
   Text,
-  FlatList,
   Image,
-  TouchableOpacity,
 } from 'react-native';
-import {useSelector} from 'react-redux';
+import {useRoute} from '@react-navigation/native';
 const BioPage = ({navigation}) => {
-  const item = useSelector(state => state?.team?.currentData);
-  console.log('item is', item);
+  console.log('item is');
   console.log('nv', navigation);
+  const route = useRoute();
+  const item = route?.params?.item;
   return (
-    <View
-      style={{
-        backgroundColor: '#56D1A7',
-        width: 280,
-        height: 177,
-        marginVertical: 12,
-        marginLeft: 32,
-        borderRadius: 12,
-        padding: 24,
-      }}>
-      <Text style={{color: '#002140', fontSize: 14, fontWeight: '600'}}>
-        {item?.category}
-      </Text>
-      <Text
+    <>
+      <View
         style={{
-          color: '#002140',
-          fontSize: 32,
-          marginTop: 16,
-          fontWeight: '700',
+          backgroundColor: '#56D1A7',
+          width: 280,
+          marginTop: 50,
+          marginLeft: 32,
+          borderRadius: 12,
+          padding: 24,
         }}>
-        {item?.name}
-      </Text>
-      <Image
-        source={{uri: item?.image}}
-        style={{
-          width: 119,
-          height: 172,
-          position: 'absolute',
-          right: -57,
-          top: -30,
-        }}
-      />
-
-      <View style={{}}>
-        <Text style={{color: '#002140', fontSize: 18, fontWeight: '600'}}>
-          {'$' + item.price}
+        <Text style={{color: '#002140', fontSize: 12, fontWeight: '600'}}>
+          {item?.category}
         </Text>
-        {/* <SvgXml xml={BagIcon} /> */}
-        {/* <SvgXml xml={Wishlist} width={100} height={100}/> */}
+        <Text
+          style={{
+            color: '#002140',
+            fontSize: 32,
+            marginTop: 16,
+            fontWeight: '700',
+          }}>
+          {item?.name}
+        </Text>
+        <Image
+          source={{uri: item?.image}}
+          style={{
+            width: 119,
+            height: 172,
+            position: 'absolute',
+            right: -57,
+            top: -30,
+          }}
+        />
+
+        <View style={{}}>
+          <Text style={{color: '#002140', fontSize: 18, fontWeight: '600'}}>
+            {'price'}
+          </Text>
+          <Text style={{color: '#002140', fontSize: 18, fontWeight: '600'}}>
+            {'$' + item.price}
+          </Text>
+        </View>
       </View>
-    </View>
+      <View style={{padding: 32}}>
+        <Text style={{color: '#002140', fontSize: 18, fontWeight: '600'}}>
+          {'Plant Bio'}
+        </Text>
+
+        <Text
+          style={{
+            color: '#002140',
+            fontSize: 15,
+            fontWeight: '400',
+            marginTop: 14,
+          }}>
+          {item?.bio}
+        </Text>
+      </View>
+    </>
   );
 };
 
--- helpify/src/Homepage.js
+++ helpify/src/Homepage.js
@@ -1,27 +1,18 @@
 import React, {useState, useEffect} from 'react';
 import {
   View,
-  Button,
   Text,
   FlatList,
   Image,
   TouchableOpacity,
 } from 'react-native';
 import axios from 'axios';
-import {SvgXml} from 'react-native-svg';
-import BagIcon from './BagIcon';
-import Wishlist from './Wishlist';
-import BioPage from './BioPage';
-import {TEAM} from './store/EntryReducer';
-import {useDispatch} from 'react-redux';
 const Homepage = ({navigation}) => {
   const [data, setData] = useState([]);
-  const dispatch = useDispatch();
   useEffect(() => {
     axios
       .get('https://create.blinkapi.io/api/eSphKNzwb9EJBt6GBjKx7Q')
       .then(response => {
-        console.log('respon', response);
         setData(response?.data);
       })
       .catch(err => {
@@ -30,7 +21,6 @@ const Homepage = ({navigation}) => {
   }, []);
 
   const BioPage = item => {
-    dispatch(TEAM({currentData: item}));
     navigation.navigate('bio', {item: item});
   };
 
@@ -75,26 +65,17 @@ const Homepage = ({navigation}) => {
         <Text style={{color: '#002140', fontSize: 18, fontWeight: '600'}}>
           {'$' + item.price}
         </Text>
-        {/* <SvgXml xml={BagIcon} /> */}
-        {/* <SvgXml xml={Wishlist} width={100} height={100}/> */}
       </View>
     </TouchableOpacity>
   );
   return (
     <View style={{backgroundColor: '#fff', minHeight: '100%'}}>
-      <Text>Welcome to Helpify</Text>
 
       <FlatList
         data={data}
         renderItem={renderItem}
         keyExtractor={item => item.id}
       />
-      <Button
-        style={{}}
-        mode="text"
-        contentStyle={{}}
-        onPress={() => {}}
-        title={'Continue as a customer'}></Button>
     </View>
   );
 };
